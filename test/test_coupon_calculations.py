bbef2c555d9e67cb79065379f5989fc93f93ad51
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-10 16:24:42 -0500

    under 50 tests

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -31,6 +31,19 @@ class FunctionTestCase(unittest.TestCase):
         result1 = coupon_calculations.calculate_price(19.50, 10, 20)
         self.assertAlmostEquals(result1, 14.36, places=1)
 
+    def test_price_under_between_thirty_fifty(self):
+        result1 = coupon_calculations.calculate_price(49.50, 5, 10)
+        self.assertAlmostEquals(result1, 55.12, places=1)
+        result1 = coupon_calculations.calculate_price(49.50, 5, 15)
+        self.assertAlmostEquals(result1, 52.77, places=1)
+        result1 = coupon_calculations.calculate_price(49.50, 5, 20)
+        self.assertAlmostEquals(result1, 50.40, places=1)
+        result1 = coupon_calculations.calculate_price(49.50, 10, 10)
+        self.assertAlmostEquals(result1, 50.35, places=1)
+        result1 = coupon_calculations.calculate_price(49.50, 10, 15)
+        self.assertAlmostEquals(result1, 48.26, places=1)
+        result1 = coupon_calculations.calculate_price(49.50, 10, 20)
+        self.assertAlmostEquals(result1, 46.16, places=1)
 
 if __name__ == '__main__':
     unittest.main()
